7a644a664cbb9c6b8eb65e12f9c536226789cede
Will T <toherwils@gmail.com>
2016-03-30 17:38:10 -0500

    Cleaned up commented code

--- index.js
+++ index.js
@@ -42,9 +42,6 @@
 			});
 		}
 
-		//var pushText;
-		//var pushUrl;
-
 		function search(query, res) {
 
 			const query_trim = query.trim();
@@ -79,29 +76,6 @@
 				return;
 			}
 
-			/*
-			if (query_split[0].toLowerCase() === 'push'){
-				res.add({
-					id: "setPushText",
-					payload: query_trim.substring(query_split[0].length + 1),
-					title: ((pushText === undefined) ? "Enter the text of your push here" : "Sending push with text: " + pushText),
-					desc: "Type the text you would like to push, select this item, and press enter."
-				});
-				res.add({
-					id: "setPushUrl",
-					payload: query_split[1],
-					title: ((pushUrl === undefined) ? "Enter your push URL here" : "Sending push with url: " + pushUrl),
-					desc: "Type the URL you would like to push, select this item, and press enter."
-				});
-				res.add({
-					id: "sendPush",
-					title: "Execute this item to send",
-					desc: "Text: \"" + pushText + "\", URL: " + pushUrl
-				});
-				return;
-			}
-			*/
-
 	    	res.add({
 	    		id: "helpText",
 	    		title: "To send a push, execute /pushbullet push [url] [description]",
@@ -129,12 +103,6 @@
 	    		});
 	    	}
 	    	});
-
-/*
-	    	if (query_trim.length === 0) {
-	      		return;
-	    	}
-*/
 		}
 
 		function loadPushes(callback){
@@ -158,10 +126,8 @@
 					body += d;
 				});
 				response.on('end', () => {
-					//logger.log(body);
 					const bodyObject = JSON.parse(body);
 					const pushes = bodyObject.pushes;
-					//const cursor = bodyObject.cursor;
 					if(bodyObject.error_code === "invalid_access_token"){
 						toast.enqueue("Something is wrong with your access token! Please reset it with /pushbullet settoken [token]");
 						saveSettings({
@@ -179,8 +145,6 @@
 
 		function execute(id, payload){
 			if(id === 'setToken'){
-				//const token = "'Access-Token: " + payload + "'";
-				//logger.log(token);
 				setToken(payload);
 				return;
 			}
@@ -275,76 +239,6 @@
 				logger.log(e.message);
 			}	
 		}
-
-		/*
-		function hashString(string){
-			// Thanks http://werxltd.com/wp/2010/05/13/javascript-implementation-of-javas-string-hashcode-method/ !
-			var hash = 0;
-			if(string.length === 0) return hash;
-			for(var i = 0; i < string.length; i++){
-				chr = string.charCodeAt(i);
-				hash = ((hash << 5)) - hash + chr; 
-				// hash << 5  ---  shift hash 5 bits to the left
-				hash = hash & hash; // Convert to 32 bit integer
-			}
-			return hash;
-		}
-
-		function sendPush(){
-			if(pushUrl === undefined && pushText === undefined){
-				toast.enqueue("Please enter either a URL or some text before sending this push");
-				return;
-			}
-
-			var push = {
-				guid: Date.now()
-			};
-
-			if(pushUrl === undefined){
-				push.type = "note";
-				push.guid *= hashString(pushText);
-				// Requires a unique guid
-			}else{
-				push.type = "link";
-				push.url = pushUrl;
-				push.guid *= hashString(pushUrl);
-			}
-
-			if(pushText !== undefined){
-				push.title = pushText;
-			}
-
-			pushJson = Json.stringify(push);
-
-			const options = {
-				"method": "POST",
-				"hostname": "api.pushbullet.com",
-				"path": "/v2/pushes",
-				"headers": {
-					"access-token": token,
-					"content-type": "application/json"
-				},
-			};
-
-			var request = https.request(options, (res) => {
-				var body = "";
-				res.on("data", (d) => {
-					body += d;
-				});
-				res.on("end", () => {
-					logger.log(body);
-				});
-			}).on('error', (e) => {
-				toast.enqueue("Unable to connect to Pushbullet. Please try again later.");
-				logger.log(e.message);
-			});
-			request.write(pushJson);
-			request.end();
-
-			toast.enqueue("Push sent successfully!");
-			app.setInput("");
-		}
-		*/
 
 		return { startup, search, execute };
 	};
